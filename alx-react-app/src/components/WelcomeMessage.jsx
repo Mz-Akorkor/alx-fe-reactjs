24042057cd4678638f81669720c167b29959e2d5
MzAkorkor <mzakorkor@gmail.com>
2025-11-02 20:49:10 +0000

    Add WelcomeMessage component and import it into App.jsx

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Welcome to My Favorite Cities App!</h1>
+      <p>Discover amazing destinations around the world.</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
