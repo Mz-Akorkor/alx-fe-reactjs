2faf3b21b0b5dc9c9fe7e2f8840aef3580980d02
MzAkorkor <mzakorkor@gmail.com>
2025-11-14 11:36:09 +0000

    Added recipe details, editing, deleting, and routing

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
 import useRecipeStore from "./recipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
-    <div style={{ padding: '20px' }}>
-      <h2>Recipes</h2>
+    <div>
       {recipes.map((recipe) => (
-        <div key={recipe.id} style={{ marginBottom: '15px' }}>
-          <h3>{recipe.title}</h3>
+        <div key={recipe.id} style={{ marginBottom: "10px" }}>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
